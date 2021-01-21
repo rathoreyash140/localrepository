e3dcfe90bc844b4ad7fedd9cc046ab4b6231405d
ADMIN <ADMIN@DESKTOP-UG17SOV>
2021-01-22 01:01:50 +0530

    Calculator related code

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,24 @@
+
+public class Calculator {
+public static void main(String[] arg)
+{
+
+	Subsctration s= new Subsctration();
+	System.out.println(s.minus(6, 2));
+	
+	Calculator c1 = new Calculator();
+	Calculator c2 = new Calculator();
+	
+	System.out.println(new Multiplication().hashCode());
+	
+	System.out.println(new Multiplication().mul(2, 3));
+	
+	System.out.println(new Multiplication().hashCode());	
+	
+	System.out.println(c1.hashCode());
+	System.out.println(c2.hashCode());
+
+	System.out.println(c1.equals(c2));
+	System.out.println(c1.equals(c1));
+}
+}
